0361c0636917163ac0e321dc0406db31f091fb34
NICNA1 <125186042+NICNA1@users.noreply.github.com>
2023-02-12 17:41:20 +0100

    Update GAUSSS.py
    me he quedado atascado al resolver

--- GAUSSS.py
+++ GAUSSS.py
@@ -1,30 +1,44 @@
-#import numpy as np
+import numpy as np
 
-A=[[1,2],[4,0]]
-B=[0,1]
-G=A
-for i in range(len(G)):
-    G[i].append(B[i])
-filas=len(G)
-columnas=len(G[0])
-print(filas,columnas)
-
-print("A=",A,"B=",B,"G=",G)
-
-"""for i in range(0,filas):
+A=np.array([[3,1],[2,2]])
+B=np.array([[1],[1]])
+G=np.append(A,B,axis=1)
+G=G.astype(np.float32)
+n=0
+S=[]
+X=[]
+shape=list((G.shape))
+filas=shape[0]
+columnas=shape[1]
+"""QUITAR 0s DE LA DIAGONAL"""
+for i in range(0,filas):
     for j in range(0,columnas):
         if i == j and G[i,j]==0:
             for k in range(0,filas):
                 if G[k,j]!=0:
-                    print(G[k],G[i],"i=",i,"k=",k)
-                    G[[]]
-                    print(turuleta,cuchufleta)
-                    print(G)"""
-                   
-        
-            
-
-    
+                    Fila1=np.copy(G[k])
+                    Fila2=np.copy(G[i])
+                    G[k]=Fila2
+                    G[i]=Fila1
+                    break
+print(G)
+for j in range(0,columnas):
+    for i in range(0,filas):
+        if i>n:
+            print(G[i]-((G[i][j]/G[n][j])*G[n]))
+            G[i]=G[i]-(G[i][j]/G[n][j])*G[n]
+    n+=1
+print(G)
+X1=G[filas-1][columnas-1]/G[filas-1][columnas-2]
+X.append(X1)
+for j in range(columnas-1,0,-1):
+    for i in range(filas-1,0,-1):
+        if i==j:
+            X.append(S[filas-1-i]/G[i][i])
+        if len(S)<filas-1:
+            S.append(G[i][columnas]-G[i][j]*X[j])
+        S[filas-1-i]
+print()(G)
         
     
 
